c6269582dcf0af98b6fbc144d41eb0bdd06d7ffd
canasmh <canasmh@yahoo.com>
2022-10-26 14:00:01 -0400

    Add introduction section to home page

---
+++ client/src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <section id='intro'>
+            <header>
+                <h1>NotMannyBugs</h1>
+                <h2>Transitioning to Tech? <br/>Let's talk about tt</h2>
+            </header>
+            <p>More Posts. Less Bugs.</p>
+        </section>
+    )
+}
+
+export default Intro;
