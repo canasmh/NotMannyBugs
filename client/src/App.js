bdb3232948c5ba52959bc54a4ddf98bd2e1d12e7
canasmh <canasmh@yahoo.com>
2022-11-17 22:39:30 -0500

    Check if screen is mobile size

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import Header from "./components/Header";
+import React, { useState , useEffect } from "react";
+import Navigation from "./components/Navigation/Navigation";
 import Intro from "./components/Intro";
 
 function App() {
+
+  const [isMobile, setIsMobile] = useState(0);
+
+    const handleResize = () => {
+        if (window.innerWidth < 768) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+      }
+      
+      // create an event listener
+      useEffect(() => {
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+      })
+
   return (
     <>
       <Header />
