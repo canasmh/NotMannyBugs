053b36c054e26a27498d08783a3a018607619d0b
canasmh <canasmh@yahoo.com>
2022-11-18 18:12:21 -0500

    Remove 'Home' from nav item and add 'study'

--- client/src/components/Navigation/NavItems.js
+++ client/src/components/Navigation/NavItems.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function NavItems() {
     return (
         <div className="nav-items">
-            <a href="#" className="nav-item">Home</a>
             <a href="#" className="nav-item">About</a>
             <a href="#" className="nav-item">Categories</a>
-            <a href="#" className="nav-item">Login</a>
+            <a href="#" className="nav-item">Study</a>
+            <a href="#" className="nav-item">Log In</a>
             <a href="#" className="nav-item">Sign Up</a>
         </div>
     )
